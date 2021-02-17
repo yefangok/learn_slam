9f21911bcb0c85c578d75fa32aa56617d6cc3d7a
yefang <yefang@daoran.tv>
2021-02-18 02:23:13 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main(int, char**) {
+    std::vector<KeyPoint> keypoints_1,keypoints_2;
+    std::cout << "Hello, world!\n";
+}
